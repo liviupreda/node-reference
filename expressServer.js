28aeabcf6eff2817ae346cd3bcb5964891de0f1b
Liviu Preda <liviupreda@gmail.com>
2019-10-19 11:54:23 +0300

    express middleware

--- expressServer.js
+++ expressServer.js
@@ -3,8 +3,14 @@ const port = 8080;
 
 const app = express();
 
+// Middleware
+app.use((req, res, next) => {
+  console.log('<h1>Middleware test</h1>');
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.send('Root');
+  res.send('Root test');
 });
 
 app.get('/profile', (req, res) => {
