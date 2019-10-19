a8a82566bc20ca4c7b1da85c483e6929d18ccca4
Liviu Preda <liviupreda@gmail.com>
2019-10-19 13:00:31 +0300

    post req, add user

--- expressServer.js
+++ expressServer.js
@@ -1,24 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = 8080;
 
 const app = express();
 
 // Middleware
-app.use((req, res, next) => {
-  console.log('<h1>Middleware test</h1>');
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   console.log('<h1>Middleware test</h1>');
+//   next();
+// });
 
 app.get('/', (req, res) => {
   res.send('Root test');
 });
 
 app.get('/profile', (req, res) => {
-  const user = {
-    name: 'Liviu',
-    hobby: 'Eating'
-  };
-  res.send(user);
+  res.send('GET profile');
+});
+
+app.post('/profile', (req, res) => {
+  console.log(req.body);
+  res.send('Added User...');
 });
 
 app.listen(port);
